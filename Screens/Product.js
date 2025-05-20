d975d5dab4a63345aae79233d685d9fe4ab9cac1
vinceprogamations <vincesdiass@gmail.com>
2025-05-20 15:34:00 -0300

    .

--- Screens/Product.js
+++ Screens/Product.js
@@ -1,22 +1,33 @@
 import { StyleSheet } from "react-native";
-import { useState } from "react";
-import { View , Text, FlatList, Image} from "react-native-web";
+import { useState, useEffect} from "react";
+import { View , Text,  FlatList, Image} from "react-native-web";
 import CardProduct from "./cardProduct";
+import { db } from "../controller";
+import {collection, getDocs} from "firebase/firestore";
 
 export default function Product(){      
 
     const [produtos, setProdutos] = useState([  // [] de array {} de dicionario
-        { id: 1, nome: 'Notebook', valor: 200.50, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 2, nome: 'Cadeira', valor: 150.75, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 3, nome: 'Smartphone', valor: 180.20, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 4, nome: 'Mesa', valor: 220.40, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 5, nome: 'Cafeteira', valor: 130.10, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 6, nome: 'Televisão', valor: 250.60, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 7, nome: 'Fritadeira', valor: 170.90, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 8, nome: 'Geladeira', valor: 200.00, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 9, nome: 'Micro-ondas', valor: 210.30, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
-        { id: 10, nome: 'Lâmpada', valor: 180.50, imagem:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWSjNaA32j96ex_Uum8plIQSYtgYQwWUHtw&s'},
     ]);
+
+    useEffect(() =>{
+        async function carregarProdutos() {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'produtos'));
+                const list = [];
+                querySnapshot.forEach((doc) => {
+                    list.push({id:doc.id, ...doc.data()});
+                });
+
+                setProdutos(list); 
+            }  catch(error){
+                 console.log('erro ao buscar o produto', error);
+            }
+
+        }
+            carregarProdutos();
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.txt}>Product</Text>
